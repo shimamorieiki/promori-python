f5f628a4034ff8ddb7532d0d88f0945d462daf7e
TKparadise <paradiseoftk@gmail.com>
2020-05-07 18:29:08 +0900

    DBに追加してみる

--- main.py
+++ main.py
@@ -7,29 +7,8 @@ import urllib.error
 import urllib.request
 import MeCab
 import re
-import sqlite3
+import usedb as db
 
-
-def add_db(table, arr):
-
-    conn = sqlite3.connect('./db/promoridb.sqlite3')
-    c = conn.cursor()
-    c.execute("insert into "+table+" values(" +
-              arr[0] + ", '" + arr[1] + "', '" + arr[2] + "', '" +
-              arr[3] + "', '" + arr[4] + "', '" + arr[5] + "')")
-    conn.commit()
-    conn.close()
-
-
-def lood_db(table):
-
-    conn = sqlite3.connect('./db/promoridb.sqlite3')
-    c = conn.cursor()
-    c.execute("select * from " + table + "')")
-    data = c.fetchall()
-    print(data)
-    conn.commit()
-    conn.close()
 
 # 自分がフォローしている人がいいねまたはリツイートした画像のうち自分がいいと思ったやつを取得
 # 自分がフォローしている人を見つける
@@ -54,7 +33,8 @@ def getApiInstance():
 def gettwitterdata(keyword, api):
     # 検索キーワード設定
     q = keyword
-    i = 0
+    sinceDate = db.latest_date("tweets")  # この日付以降のツイートを取得する
+    # i = 0
     meishi = []
     doushi = []
 
@@ -65,82 +45,63 @@ def gettwitterdata(keyword, api):
     for tweet in tweepy.Cursor(api.search,
                                q=q,
                                count=100,
-                               tweet_mode='extended'
+                               tweet_mode='extended',
+                               since=sinceDate  # <-追加
                                ).items():
 
-        if (tweet.created_at <
-                datetime.datetime.now() + datetime.timedelta(hours=8)):
-            meishi = []
-            doushi = []
-            # print(tweet.user.screen_name)
-            # print(tweet.user.protected)
-            # print(tweet.user.followers_count)
-            # print(tweet.user.friends_count)
-            # print(tweet.created_at + datetime.timedelta(hours=9))
-            # print(tweet.created_at)
-            # print(tweet.user.favourites_count)
-            strtext = (tweet.full_text.replace("#どうぶつの森", "")
-                       .replace("#AnimalCrossing", "")
-                       .replace("#ACNH", "")
-                       .replace("#NintendoSwitch", ""))
-            strtext = re.sub(
-                'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+', "", strtext)
-            m = MeCab.Tagger("-Ochasen -u ./dic/atsumori20200507.dic")
-            for line in strtext.split("\n"):
-                for kore in m.parse(line).split("\t\t"):
-                    # print(kore)
-                    linelist = kore.split()
-                    # print(linelist)
-                    if len(linelist) >= 3:
-                        if "-" in linelist[3]:
-                            pos = linelist[3].split("-")
-                            if pos[0] == "名詞":
-                                meishi.append(linelist[2])
-                            elif pos[0] == "動詞":
-                                doushi.append(linelist[2])
+        meishi = []
+        doushi = []
+        # print(tweet.user.screen_name)
+        # print(tweet.user.protected)
+        # print(tweet.user.followers_count)
+        # print(tweet.user.friends_count)
+        # print(tweet.created_at + datetime.timedelta(hours=9))
+        # print(tweet.created_at)
+        # print(tweet.user.favourites_count)
+        strtext = (tweet.full_text.replace("#どうぶつの森", "")
+                   .replace("#AnimalCrossing", "")
+                   .replace("#ACNH", "")
+                   .replace("#NintendoSwitch", ""))
+        strtext = re.sub(
+            'https?://[\w/:%#\$&\?\(\)~\.=\+\-]+', "", strtext)
+        m = MeCab.Tagger("-Ochasen -u ./dic/atsumori20200507.dic")
+        for line in strtext.split("\n"):
+            for kore in m.parse(line).split("\t\t"):
+                # print(kore)
+                linelist = kore.split()
+                # print(linelist)
+                if len(linelist) >= 3:
+                    if "-" in linelist[3]:
+                        pos = linelist[3].split("-")
+                        if pos[0] == "名詞":
+                            meishi.append(linelist[2])
+                        elif pos[0] == "動詞":
+                            doushi.append(linelist[2])
 
-            if len(meishi) != 0 and len(doushi) != 0:
-                # print(tweet.full_text)
-                if (("求" or "交換" or "譲" or "出" or "お礼", "余") in meishi or
-                        ("求める"or"交換する"or"譲る"or"出す"or"頂く"or"いただく"or"余る") in doushi):
-                    i = i + 1
-                    # print("これは交換希望のツイート")
-                    # print("ユーザー:"+tweet.user.name)
-                    print(strtext)
-                    print(meishi)
-                    print(doushi)
+        if len(meishi) != 0 and len(doushi) != 0:
+            # print(tweet.full_text)
+            if (("求" or "交換" or "譲" or "出" or "お礼", "余") in meishi or
+                    ("求める"or"交換する"or"譲る"or"出す"or"頂く"or"いただく"or"余る") in doushi):
+                # i = i + 1
+                # print("これは交換希望のツイート")
+                # print("ユーザー:"+tweet.user.name)
+                # print(strtext)
+                # print(meishi)
+                # print(doushi)
 
-                    # 今からここでほしいものと渡すものの特定を行いたい
+                # 今からここでほしいものと渡すものの特定を行いたい
 
-                    # u_n = tweet.user.screen_name
-                    # f_t = tweet.full_text
-                    # c_a = str(tweet.created_at)
-                    # t_id = str(tweet.id)
-                    # ret = ('<blockquote class="twitter-tweet">' +
-                    #        '<p lang="ja" dir="ltr">' + f_t + '</p>' +
-                    #        '&mdash; ' + tweet.user.name + '(@' + u_n + ')' +
-                    #        ' <a href="https://twitter.com/' + u_n+'/status/' +
-                    #        t_id + '?ref_src=twsrc%5Etfw">' + c_a + '</a>' +
-                    #        '</blockquote>' +
-                    #        '<script async src="https://platform.twitter.com/widgets.js" charset="utf-8">' +
-                    #        '</script>'
-                    #        )
+                u_n = tweet.user.screen_name
+                f_t = tweet.full_text
+                c_a = str(tweet.created_at)
+                t_id = str(tweet.id)
 
-                    # with open("./text/honto.txt", mode='a', encoding="utf-8") as f:
-                    #     f.write(ret)
-                    #     print(i)
-                else:
-                    pass
-                    # print("これは交換とは関係ないツイート")
-                    # print("ユーザー:"+tweet.user.name)
-                    # print(strtext)
-
-            if i > 20:
-                break
-
-        else:
-            print("これどうなの")
-            break
+                created_time = (c_a.replace("-", "")
+                                .replace(":", "")
+                                .replace(" ", ""))
+                # print(time)
+                arr = [t_id, u_n, created_time, '', '', f_t]
+                db.add_db('tweets', arr)
 
 
 if __name__ == '__main__':
--- usedb.py
+++ usedb.py
@@ -1,9 +1,58 @@
 import sqlite3
-conn = sqlite3.connect('./db/promoridb.sqlite3')
-c = conn.cursor()
-c.execute("insert into tweets values(2, '太郎', '20200507', '', '', '')")
-c.execute('select * from tweets')
-data = c.fetchall()
-print(data)
-conn.commit()
-conn.close()
+from datetime import datetime as dt
+
+
+def add_db(table, arr):
+
+    conn = sqlite3.connect('./db/promoridb.sqlite3')
+    c = conn.cursor()
+    c.execute("insert into "+table+" values(" +
+              arr[0] + ", '" + arr[1] + "', '" + arr[2] + "', '" +
+              arr[3] + "', '" + arr[4] + "', '" + arr[5] + "')")
+    conn.commit()
+    conn.close()
+
+
+def look_db_all(table):
+
+    conn = sqlite3.connect('./db/promoridb.sqlite3')
+    c = conn.cursor()
+    c.execute("select * from " + table)
+    data = c.fetchall()
+    print(data)
+    conn.commit()
+    conn.close()
+
+
+def look_db_itr(table):
+
+    conn = sqlite3.connect('./db/promoridb.sqlite3')
+    c = conn.cursor()
+    for row in c.execute("select * from " + table):
+        print(row)
+    conn.commit()
+    conn.close()
+
+
+def latest_date(table):
+    conn = sqlite3.connect('./db/promoridb.sqlite3')
+    c = conn.cursor()
+    for row in c.execute("select * from " + table+" order by t_time desc"):
+        ns = str(row[2])
+        break
+    conn.commit()
+    conn.close()
+    year = ns[0] + ns[1] + ns[2] + ns[3]
+    month = ns[4] + ns[5]
+    day = ns[6] + ns[7]
+    hour = ns[8] + ns[9]
+    minute = ns[10] + ns[11]
+    second = ns[12] + ns[13]
+    tstr = year + "-"+month+"-"+day+" "+hour+":"+minute+":"+second
+    tdatetime = dt.strptime(tstr, '%Y-%m-%d %H:%M:%S')
+    return tdatetime
+
+
+if __name__ == "__main__":
+    # look_db_itr("tweets")
+    print(latest_date("tweets"))
